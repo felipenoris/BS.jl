1604556d445cb6b945ff4e325b8ff8cfa1380c63
Felipe Noronha <felipenoris@gmail.com>
2019-02-17 00:20:26 -0300

    Greeks

--- src/BS.jl
+++ src/BS.jl
@@ -3,20 +3,28 @@ module BS
 
 using Distributions
 
+#
+# types
+#
+
 struct D₁Parts{T}
     numerator::T
     denominator::T
 end
 
 abstract type BSOption end
-struct BSCall <: BSOption end
-struct BSPut <: BSOption end
 
-@inline N(x) = cdf(Normal(), x)
+"European call option."
+struct EuropeanCall <: BSOption end
+
+"European put option."
+struct EuropeanPut <: BSOption end
 
 #
 # asset pays no dividends
 #
+
+@inline N(x) = cdf(Normal(), x)
 
 @inline D₁Parts(s, k, r, σ, t) = D₁Parts(log(s/k) + (r + σ^2/2 )*t, σ*sqrt(t))
 
@@ -26,18 +34,18 @@ struct BSPut <: BSOption end
 @inline d₁(s, k, r, σ, t) = d₁(D₁Parts(s, k, r, σ, t))
 @inline d₂(s, k, r, σ, t) = d₂(D₁Parts(s, k, r, σ, t))
 
-@inline function bscall(s, k, r, σ, t)
+@inline function call(s, k, r, σ, t)
     parts = D₁Parts(s, k, r, σ, t)
     return s*N(d₁(parts)) - k*exp(-r*t)*N(d₂(parts))
 end
 
-@inline function bsput(s, k, r, σ, t)
+@inline function put(s, k, r, σ, t)
     parts = D₁Parts(s, k, r, σ, t)
     return k*exp(-r*t)*N(-d₂(parts)) - s*N(-d₁(parts))
 end
 
-@inline bsprice(::BSCall, s, k, r, σ, t) = bscall(s, k, r, σ, t)
-@inline bsprice(::BSPut, s, k, r, σ, t) = bsput(s, k, r, σ, t)
+@inline price(::EuropeanCall, s, k, r, σ, t) = call(s, k, r, σ, t)
+@inline price(::EuropeanPut, s, k, r, σ, t) = put(s, k, r, σ, t)
 
 #
 # asset pays dividends at rate q
@@ -48,10 +56,44 @@ end
 @inline d₁(s, k, r, σ, t, q) = d₁(D₁Parts(s, k, r, σ, t, q))
 @inline d₂(s, k, r, σ, t, q) = d₂(D₁Parts(s, k, r, σ, t, q))
 
-@inline bscall(s, k, r, σ, t, q) = bscall(s*exp(-q*t), k, r, σ, t)
-@inline bsput(s, k, r, σ, t, q) = bsput(s*exp(-q*t), k, r, σ, t)
+@inline call(s, k, r, σ, t, q) = call(s*exp(-q*t), k, r, σ, t)
+@inline put(s, k, r, σ, t, q) = put(s*exp(-q*t), k, r, σ, t)
 
-@inline bsprice(::BSCall, s, k, r, σ, t, q) = bscall(s, k, r, σ, t, q)
-@inline bsprice(::BSPut, s, k, r, σ, t, q) = bsput(s, k, r, σ, t, q)
+@inline price(::EuropeanCall, s, k, r, σ, t, q) = call(s, k, r, σ, t, q)
+@inline price(::EuropeanPut, s, k, r, σ, t, q) = put(s, k, r, σ, t, q)
+
+#
+# Greeks
+#
+
+@inline delta(::EuropeanCall, s, k, r, σ, t) = N(d₁(s, k, r, σ, t))
+@inline delta(::EuropeanPut, s, k, r, σ, t) = N(d₁(s, k, r, σ, t)) - 1
+
+@inline function theta(::EuropeanCall, s, k, r, σ, t)
+    parts = D₁Parts(s, k, r, σ, t)
+    return -(s * pdf(Normal(), d₁(parts)) * σ) / ( 2*sqrt(t) ) - r*k*exp(-r*t)*N(d₂(parts))
+end
+
+@inline function theta(::EuropeanPut, s, k, r, σ, t)
+    parts = D₁Parts(s, k, r, σ, t)
+    return -(s * pdf(Normal(), d₁(parts)) * σ) / ( 2*sqrt(t) ) + r*k*exp(-r*t)*N(-d₂(parts))
+end
+
+@inline function gamma(::BSOption, s, k, r, σ, t)
+    parts = D₁Parts(s, k, r, σ, t)
+    return pdf(Normal(), d₁(parts)) / (s * parts.denominator)
+end
+
+@inline function vega(::BSOption, s, k, r, σ, t)
+    return s*sqrt(t)*pdf(Normal(), d₁(s, k, r, σ, t))
+end
+
+@inline function rho(::EuropeanCall, s, k, r, σ, t)
+    return k*t*exp(-r*t)*N(d₂(s, k, r, σ, t))
+end
+
+@inline function rho(::EuropeanPut, s, k, r, σ, t)
+    return -k*t*exp(-r*t)*N(-d₂(s, k, r, σ, t))
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,62 +2,99 @@
 import BS
 using Test
 
-@testset "bs price" begin
 
-    let
-        s = 42
-        k = 40
-        r = 0.1
-        sigma = 0.2
-        t = 0.5
+@testset "BS Price" begin
+    @testset "no dividends" begin
 
-        @test BS.bscall(s, k, r, sigma, t) ≈ 4.759422392871542
-        @test BS.bsput(s, k, r, sigma, t) ≈ 0.8085993729000975
-        @test BS.bsprice(BS.BSCall(), s, k, r, sigma, t) ≈ 4.759422392871542
-        @test BS.bsprice(BS.BSPut(), s, k, r, sigma, t) ≈ 0.8085993729000975
+        let
+            s = 42
+            k = 40
+            r = 0.1
+            sigma = 0.2
+            t = 0.5
+
+            @test BS.call(s, k, r, sigma, t) ≈ 4.759422392871542
+            @test BS.put(s, k, r, sigma, t) ≈ 0.8085993729000975
+            @test BS.price(BS.EuropeanCall(), s, k, r, sigma, t) ≈ 4.759422392871542
+            @test BS.price(BS.EuropeanPut(), s, k, r, sigma, t) ≈ 0.8085993729000975
+        end
+
+        let
+            s = 40.0
+            k = 40.0
+            r = 0.1
+            t = 0.5
+            sigma = 0.3
+
+            @test BS.call(s, k, r, sigma, t) ≈ 4.3625999408029585
+            @test BS.put(s, k, r, sigma, t) ≈ 2.4117769208315245
+            @test BS.price(BS.EuropeanCall(), s, k, r, sigma, t) ≈ 4.3625999408029585
+            @test BS.price(BS.EuropeanPut(), s, k, r, sigma, t) ≈ 2.4117769208315245
+        end
     end
 
-    let
-        s = 40.0
-        k = 40.0
-        r = 0.1
-        t = 0.5
-        sigma = 0.3
+    @testset "dividends q" begin
 
-        @test BS.bscall(s, k, r, sigma, t) ≈ 4.3625999408029585
-        @test BS.bsput(s, k, r, sigma, t) ≈ 2.4117769208315245
-        @test BS.bsprice(BS.BSCall(), s, k, r, sigma, t) ≈ 4.3625999408029585
-        @test BS.bsprice(BS.BSPut(), s, k, r, sigma, t) ≈ 2.4117769208315245
+        let
+            s = 930
+            k = 900
+            r = 0.08
+            sigma = 0.2
+            t = 2/12
+            q = 0.03
+
+            @test BS.call(s, k, r, sigma, t, q) ≈ 51.83295679649086
+            @test BS.put(s, k, r, sigma, t, q) ≈ 14.550996773772397
+            @test BS.price(BS.EuropeanCall(), s, k, r, sigma, t, q) ≈ 51.83295679649086
+            @test BS.price(BS.EuropeanPut(), s, k, r, sigma, t, q) ≈ 14.550996773772397
+        end
+
+        let
+            s = 1.8
+            k = 1.8
+            r = 0.1
+            rf = 0.03
+            t = 0.5
+            sigma = 0.2
+
+            @test BS.call(s, k, r, sigma, t, rf) ≈ 0.13172208280604858
+            @test BS.put(s, k, r, sigma, t, rf) ≈ 0.07073355562182104
+            @test BS.price(BS.EuropeanCall(), s, k, r, sigma, t, rf) ≈ 0.13172208280604858
+            @test BS.price(BS.EuropeanPut(), s, k, r, sigma, t, rf) ≈ 0.07073355562182104
+        end
     end
 end
 
-@testset "bs price dividends q" begin
+@testset "Greeks" begin
 
-    let
-        s = 930
-        k = 900
-        r = 0.08
-        sigma = 0.2
-        t = 2/12
-        q = 0.03
+    s = 49
+    k = 50
+    r = 0.05
+    t = 0.3846
+    sigma = 0.2
 
-        @test BS.bscall(s, k, r, sigma, t, q) ≈ 51.83295679649086
-        @test BS.bsput(s, k, r, sigma, t, q) ≈ 14.550996773772397
-        @test BS.bsprice(BS.BSCall(), s, k, r, sigma, t, q) ≈ 51.83295679649086
-        @test BS.bsprice(BS.BSPut(), s, k, r, sigma, t, q) ≈ 14.550996773772397
+    @testset "Delta" begin
+        @test BS.delta(BS.EuropeanCall(), s, k, r, sigma, t) ≈ 0.5216016339715761
+        @test BS.delta(BS.EuropeanPut(), s, k, r, sigma, t) ≈ -0.4783983660284239
     end
 
-    let
-        s = 1.8
-        k = 1.8
-        r = 0.1
-        rf = 0.03
-        t = 0.5
-        sigma = 0.2
+    @testset "Theta" begin
+        @test BS.theta(BS.EuropeanCall(), s, k, r, sigma, t) ≈ -4.305389964546104
+        @test BS.theta(BS.EuropeanPut(), s, k, r, sigma, t) ≈ -1.853005672196868
+    end
 
-        @test BS.bscall(s, k, r, sigma, t, rf) ≈ 0.13172208280604858
-        @test BS.bsput(s, k, r, sigma, t, rf) ≈ 0.07073355562182104
-        @test BS.bsprice(BS.BSCall(), s, k, r, sigma, t, rf) ≈ 0.13172208280604858
-        @test BS.bsprice(BS.BSPut(), s, k, r, sigma, t, rf) ≈ 0.07073355562182104
+    @testset "Gamma" begin
+        @test BS.gamma(BS.EuropeanCall(), s, k, r, sigma, t) ≈ 0.06554537725247868
+        @test BS.gamma(BS.EuropeanPut(), s, k, r, sigma, t) ≈ 0.06554537725247868
+    end
+
+    @testset "Vega" begin
+        @test BS.vega(BS.EuropeanCall(), s, k, r, sigma, t) ≈ 12.105242754243843
+        @test BS.vega(BS.EuropeanPut(), s, k, r, sigma, t) ≈ 12.105242754243843
+    end
+
+    @testset "Rho" begin
+        @test BS.rho(BS.EuropeanCall(), s, k, r, sigma, t) ≈ 8.906574098800945
+        @test BS.rho(BS.EuropeanPut(), s, k, r, sigma, t) ≈ -9.95716587794938
     end
 end
