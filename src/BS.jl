0f34c7b3e1b61139061cd1c51931a3e47c529a5a
Felipe Noronha <felipenoris@gmail.com>
2019-02-17 00:00:34 -0300

    BS price with dividends

--- src/BS.jl
+++ src/BS.jl
@@ -14,6 +14,10 @@ struct BSPut <: BSOption end
 
 @inline N(x) = cdf(Normal(), x)
 
+#
+# asset pays no dividends
+#
+
 @inline D₁Parts(s, k, r, σ, t) = D₁Parts(log(s/k) + (r + σ^2/2 )*t, σ*sqrt(t))
 
 @inline d₁(parts::D₁Parts) = parts.numerator / parts.denominator
@@ -34,5 +38,20 @@ end
 
 @inline bsprice(::BSCall, s, k, r, σ, t) = bscall(s, k, r, σ, t)
 @inline bsprice(::BSPut, s, k, r, σ, t) = bsput(s, k, r, σ, t)
+
+#
+# asset pays dividends at rate q
+#
+
+@inline D₁Parts(s, k, r, σ, t, q) = D₁Parts(s*exp(-q*t), k, r, σ, t)
+
+@inline d₁(s, k, r, σ, t, q) = d₁(D₁Parts(s, k, r, σ, t, q))
+@inline d₂(s, k, r, σ, t, q) = d₂(D₁Parts(s, k, r, σ, t, q))
+
+@inline bscall(s, k, r, σ, t, q) = bscall(s*exp(-q*t), k, r, σ, t)
+@inline bsput(s, k, r, σ, t, q) = bsput(s*exp(-q*t), k, r, σ, t)
+
+@inline bsprice(::BSCall, s, k, r, σ, t, q) = bscall(s, k, r, σ, t, q)
+@inline bsprice(::BSPut, s, k, r, σ, t, q) = bsput(s, k, r, σ, t, q)
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -13,6 +13,8 @@ using Test
 
         @test BS.bscall(s, k, r, sigma, t) ≈ 4.759422392871542
         @test BS.bsput(s, k, r, sigma, t) ≈ 0.8085993729000975
+        @test BS.bsprice(BS.BSCall(), s, k, r, sigma, t) ≈ 4.759422392871542
+        @test BS.bsprice(BS.BSPut(), s, k, r, sigma, t) ≈ 0.8085993729000975
     end
 
     let
@@ -24,5 +26,38 @@ using Test
 
         @test BS.bscall(s, k, r, sigma, t) ≈ 4.3625999408029585
         @test BS.bsput(s, k, r, sigma, t) ≈ 2.4117769208315245
+        @test BS.bsprice(BS.BSCall(), s, k, r, sigma, t) ≈ 4.3625999408029585
+        @test BS.bsprice(BS.BSPut(), s, k, r, sigma, t) ≈ 2.4117769208315245
+    end
+end
+
+@testset "bs price dividends q" begin
+
+    let
+        s = 930
+        k = 900
+        r = 0.08
+        sigma = 0.2
+        t = 2/12
+        q = 0.03
+
+        @test BS.bscall(s, k, r, sigma, t, q) ≈ 51.83295679649086
+        @test BS.bsput(s, k, r, sigma, t, q) ≈ 14.550996773772397
+        @test BS.bsprice(BS.BSCall(), s, k, r, sigma, t, q) ≈ 51.83295679649086
+        @test BS.bsprice(BS.BSPut(), s, k, r, sigma, t, q) ≈ 14.550996773772397
+    end
+
+    let
+        s = 1.8
+        k = 1.8
+        r = 0.1
+        rf = 0.03
+        t = 0.5
+        sigma = 0.2
+
+        @test BS.bscall(s, k, r, sigma, t, rf) ≈ 0.13172208280604858
+        @test BS.bsput(s, k, r, sigma, t, rf) ≈ 0.07073355562182104
+        @test BS.bsprice(BS.BSCall(), s, k, r, sigma, t, rf) ≈ 0.13172208280604858
+        @test BS.bsprice(BS.BSPut(), s, k, r, sigma, t, rf) ≈ 0.07073355562182104
     end
 end
